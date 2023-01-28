0849b3dcb8498ef38f5df52d7690cd948d3a83f3
nikamir18 <103753339+nikamir18@users.noreply.github.com>
2023-01-28 20:48:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  console.log()
+
+  const Modal =({ open, setOpen, children })=>
+  
+  (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+        <div className="modal">
+          <svg
+            onClick={() => setOpen(false)}
+            height="200"
+            viewBox="0 0 200 200"
+            width="200"
+          >
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          {children}
+        </div>
+      </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>Это модальное окно</h3>
+      </Modal>
     </div>
+   
   );
 }
 
